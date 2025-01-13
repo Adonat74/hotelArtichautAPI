14e9ef64624be9260fec5252d04df0bece1410b1
Adonat74 <donatgoninet.antoine@gmail.com>
2025-01-13 17:01:55 +0100

    made the model but stille neede to be finalised

--- app/Models/Content.php
+++ app/Models/Content.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Content extends Model
 {
-    //
+    /** @use HasFactory<\Database\Factories\ContentFactory> */
+    use HasFactory;
+
+    protected $primaryKey = 'flight_id';
+
+
+    protected $fillable = [
+        'name',
+        'title',
+        'short_description',
+        'description',
+        'landing_page_display',
+        'navbar_display',
+        'link',
+    ];
 }
