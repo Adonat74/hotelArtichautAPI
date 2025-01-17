64b558d502251cb03a9d3446a1dbad1379973511
Adonat74 <donatgoninet.antoine@gmail.com>
2025-01-17 16:13:27 +0100

    renamed the files

--- database/seeders/RoomCategoryFeatureSeeder.php
+++ database/seeders/RoomCategoryFeatureSeeder.php
@@ -20,7 +20,7 @@ class RoomCategoryFeatureSeeder extends Seeder
             // Associez 1 à 5 fonctionnalités aléatoires à chaque catégorie
             $features = $roomFeatures->random(rand(1, 3))->pluck('id');
             foreach ($features as $featureId) {
-                DB::table('room_category_feature')->insert([
+                DB::table('rooms_categories_features')->insert([
                     'rooms_category_id' => $category->id,
                     'rooms_feature_id' => $featureId,
                 ]);
