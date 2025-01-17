0c8eae8fe7af40b8c24a510d67572aa499ed3d7c
Hamtidiane <hamtidiane@gmail.com>
2025-01-17 09:53:31 +0100

    categoryfeature

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
             RoomsSeeder::class,
             RoomsFeatureSeeder::class,
             ReviewsSeeder::class,
+            RoomCategoryFeatureSeeder::class,
         ]);
 
     }
