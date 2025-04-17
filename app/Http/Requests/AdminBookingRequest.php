aef3af8ba63324a027efd3c075d2a126b7ffcd43
Adonat74 <donatgoninet.antoine@gmail.com>
2025-04-17 16:52:40 +0200

    removed the payment routes and added some .http file routes to test

--- app/Http/Requests/AdminBookingRequest.php
+++ app/Http/Requests/AdminBookingRequest.php
@@ -17,8 +17,6 @@ class AdminBookingRequest extends FormRequest
         return [
             'check_in' => 'bail|required|date|after:today',
             'check_out' => 'bail|required|date|after:check_in',
-            'total_price_in_cent' => 'bail|required|integer',
-            'to_be_paid_in_cent' => 'bail|required|integer',
             'user_id' => 'bail|required|exists:users,id',
             'rooms' => 'bail|required|array',
             'rooms.*' => 'bail|required|exists:rooms,id',
